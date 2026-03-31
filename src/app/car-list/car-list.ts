2fb08099bf77129ad20349711c8bb6b732550314
kanantechhub <w0878434@myscc.ca>
2026-03-31 14:18:25 -0400

    Fixed AppComponent naming and import extensions

--- src/app/car-list/car-list.ts
+++ src/app/car-list/car-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Content } from '../models/content'; 
 
 @Component({
   selector: 'app-car-list',
+  imports: [CommonModule],
   templateUrl: './car-list.html',
   styleUrls: ['./car-list.css']
 })
