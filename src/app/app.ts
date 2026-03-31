4d75243d57fbd59e262a358d22c89f06334ae782
kanantechhub <w0878434@myscc.ca>
2026-03-31 12:58:42 -0400

    content interface created , displaying car data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Content } from './models/content'; // Importing our rules
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.html', // This points to your HTML file
+  styleUrls: ['./app.css']    // This points to your CSS file
 })
-export class App {
-  protected readonly title = signal('Kanan_Kaur_Learning_Angular');
+export class AppComponent {
+  // We create a single car object here
+  myCar: Content = {
+    id: 1,
+    title: 'Dodge Challenger',
+    description: 'A powerful modern muscle car with a classic look.',
+    creator: 'Dodge',
+    type: 'Muscle Car',
+    imgURL: 'https://images.unsplash.com/photo-1603386329225-868f9b1ee6c9'
+  };
 }
